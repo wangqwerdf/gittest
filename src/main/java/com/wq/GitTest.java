23e288532e88ff7c3b6dadc9c576705332d33bc9
wq <you@example.com>
2022-11-13 11:40:04 +0800

    first commit

---
+++ src/main/java/com/wq/GitTest.java
@@ -0,0 +1,12 @@
+package com.wq;
+
+/**
+ * @author:汪强
+ * @creat:2022-11-13-11:35
+ * @description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
